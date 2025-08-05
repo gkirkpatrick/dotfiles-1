aebc5428a27c1067bc6f0fd123b21d08335de7cf
Greg Kirkpatrick <gkirkpatrick@users.noreply.github.com>
2025-08-05 14:48:18 -0400

    Update embedded pyflakes to 3.4.0

--- vim/bundle/pyflakes-vim/ftplugin/python/pyflakes/pyflakes/test/harness.py
+++ vim/bundle/pyflakes-vim/ftplugin/python/pyflakes/pyflakes/test/harness.py
@@ -1,27 +1,34 @@
-
+import ast
 import textwrap
-import _ast
-
-from twisted.trial import unittest
+import unittest
 
 from pyflakes import checker
 
+__all__ = ['TestCase', 'skip', 'skipIf']
 
-class Test(unittest.TestCase):
+skip = unittest.skip
+skipIf = unittest.skipIf
+
+
+class TestCase(unittest.TestCase):
+
+    withDoctest = False
 
     def flakes(self, input, *expectedOutputs, **kw):
-        ast = compile(textwrap.dedent(input), "<test>", "exec",
-                      _ast.PyCF_ONLY_AST)
-        w = checker.Checker(ast, **kw)
+        tree = ast.parse(textwrap.dedent(input))
+        if kw.get('is_segment'):
+            tree = tree.body[0]
+            kw.pop('is_segment')
+        w = checker.Checker(tree, withDoctest=self.withDoctest, **kw)
         outputs = [type(o) for o in w.messages]
         expectedOutputs = list(expectedOutputs)
-        outputs.sort()
-        expectedOutputs.sort()
-        self.assert_(outputs == expectedOutputs, '''\
+        outputs.sort(key=lambda t: t.__name__)
+        expectedOutputs.sort(key=lambda t: t.__name__)
+        self.assertEqual(outputs, expectedOutputs, '''\
 for input:
-%s
+{}
 expected outputs:
-%s
+{!r}
 but got:
-%s''' % (input, repr(expectedOutputs), '\n'.join([str(o) for o in w.messages])))
+{}'''.format(input, expectedOutputs, '\n'.join([str(o) for o in w.messages])))
         return w
